6728333905d4a79e618263c2c7c6eb6b114ba58a
UJJWAL SHARMA <92508362+ujjwal0-2@users.noreply.github.com>
2024-09-28 15:19:17 +0530

    Create if-else.cpp

---
+++ if-else.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+  int marks ;
+  cin >> marks ;
+  if(marks >=90){
+     cout<<"Grade A"<<endl;
+  }
+  else if(marks >=80){
+    cout<<"Grade B"<<endl;
+    
+  }
+  else{
+    cout<<"Fail"<<endl;
+  }
+}
