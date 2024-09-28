51a25a94a04f281524262cd3fd9aeb82bb8452f8
UJJWAL SHARMA <92508362+ujjwal0-2@users.noreply.github.com>
2024-09-28 16:02:12 +0530

    Update MCQ.cpp

--- MCQ.cpp
+++ MCQ.cpp
@@ -9,3 +9,15 @@ if(cin>>n){
 }
  
 } /// It will definiatly run fine
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+
+if(cout<<"ujj"){
+    cout <<"ujjwal"<<endl;
+}
+ 
+}/// It will definiatly run fine
