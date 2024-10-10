c3ad07e03e3b46e0ab23bf4510fc6e63a828b131
UJJWAL SHARMA <92508362+ujjwal0-2@users.noreply.github.com>
2024-10-10 18:53:29 +0530

    Create function.cpp

---
+++ function.cpp
@@ -0,0 +1,14 @@
+void myFunction() {
+  cout << "I just got executed!\n";
+}
+
+int main() {
+  myFunction();
+  myFunction();
+  myFunction();
+  return 0;
+}
+
+// I just got executed!
+// I just got executed!
+// I just got executed!
