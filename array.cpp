1d8f1d48a731cbdd87cf487246e501f394a089de
UJJWAL SHARMA <92508362+ujjwal0-2@users.noreply.github.com>
2025-01-10 00:16:21 +0530

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,16 @@
+// Print an  Basic  Array
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, World!";
+    cout <<endl;
+    
+    int arr[3]={2,5,6};
+    for(int i=0; i<=3;i++){
+      cout<< arr[i]<<" ";
+    }
+    return 0;
+}
